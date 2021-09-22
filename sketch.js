a0ac70800675b118b69d966591e6fa18250ff484
Stephanie Jacobo <52087398+stephonme@users.noreply.github.com>
2021-09-22 19:26:31 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+
+
+
+var r = 100;
+
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+}
+
+function draw() {
+  // put drawing code here
+
+  background(255, 255, 0);
+
+
+
+
+  fill(255, 0, mouseX);
+  ellipse(windowWidth/2 ,windowHeight/2 ,100 ,100);
+  // variables automatically scale when you use math functions
+  // window with depends on the screens width and how the
+  // user is manipulating the screen
+
+  // text(mouseX, 50, 50);
+
+  //if(mouseIsPressed){
+
+  //ellipse(mouseX ,mouseY ,100 ,100);
+  //Mouse X and mouseY follow the clicker
+//}
+  //else {
+
+    //rect(100,100,100,100);
+
+    }
+}
+
+
+//function mousePressed(){
+//}
